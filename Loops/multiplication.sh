468b06052f0da20a225f56160d8ecb3c1bfb0359
mohan <mohanperiyasamyap@gmail.com>
2026-03-06 07:47:16 +0530

    commiting new

---
+++ Loops/multiplication.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {1..10}
+do
+d=$(( i * 5 ))
+echo "$i * 5 = $d"
+done
---
+++ Loops/simpleloop3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in {1..10}
+do
+if [ $((i % 2)) -ne 0 ]
+then
+echo "odd value $i"
+fi
+done
